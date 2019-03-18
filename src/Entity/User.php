fe7f450951305baa62687898d0cfe8399b865fc6
James Judge <jamesj@apparelbusiness.com>
2019-03-18 16:28:56 -0400

    migrations and user class

---
+++ src/Entity/User.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     * @Assert\NotBlank
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     * @Assert\NotBlank
+     */
+    private $passwordHash;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nickname;
+
+    /**
+     * @ORM\Column(type="string", length=250, nullable=true)
+     */
+    private $streamingKey;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPasswordHash(): ?string
+    {
+        return $this->passwordHash;
+    }
+
+    public function setPasswordHash(string $passwordHash): self
+    {
+
+        $this->passwordHash = $passwordHash;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function checkEmailExists($email){
+
+        return false;
+    }
+
+    public function checkNicknameExists($nickname){
+
+        return false;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): self
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    public function getStreamingKey(): ?string
+    {
+        return $this->streamingKey;
+    }
+
+    public function setStreamingKey(?string $streamingKey): self
+    {
+        $this->streamingKey = $streamingKey;
+
+        return $this;
+    }
+
+
+
+
+}
---
+++ src/Migrations/Version20190318163235.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190318163235 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX idxNickname ON user');
+        $this->addSql('DROP INDEX idxEmail ON user');
+        $this->addSql('ALTER TABLE user ADD streaming_key VARCHAR(250) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP streaming_key');
+        $this->addSql('CREATE UNIQUE INDEX idxNickname ON user (id)');
+        $this->addSql('CREATE UNIQUE INDEX idxEmail ON user (email)');
+    }
+}
