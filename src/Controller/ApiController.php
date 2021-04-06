e05d26d5b382227a5212f00535d1b0c11bdc3a02
James Judge <jjudge.developer83@gmail.com>
2021-04-06 16:26:12 -0400

    pfffffffffffffffffft

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\User;
+use JMS\Serializer\SerializerBuilder;
 
 
 /**
